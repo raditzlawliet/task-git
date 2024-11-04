6e692491beac1a1f89e35d2579e1713a5c90e294
Radityo <radityohernanda@gmail.com>
2024-11-04 14:25:27 +0700

    init Project

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"go-basic/utils"
+)
+
+func main() {
+	fmt.Println(utils.MagicSum(10))
+
+	fmt.Println(utils.MagicSum(20))
+}
---
+++ utils/magic.go
@@ -0,0 +1,9 @@
+package utils
+
+func MagicSum(n int) int {
+	return n + n
+}
+
+func magicSum2(n int) int {
+	return n + n
+}
